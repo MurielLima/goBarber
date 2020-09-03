3266c6b74ec605e55a8640cf31bff5c8ede05022
Muriel Lima <muriellima2@gmail.com>
2020-09-03 18:01:47 -0300

    Add files via upload

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, StatusBar} from 'react-native';
+
+const signUp: React.FC = () => {
+  return (
+    <>
+      <View style={{flex: 1, backgroundColor: '#312e38'}}></View>
+    </>
+  );
+};
+
+export default signUp;
